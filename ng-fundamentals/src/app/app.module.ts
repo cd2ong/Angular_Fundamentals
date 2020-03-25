a137e7c2ab359712341c4ecaf79baad968059786
cd2ong <c.david.ong@gmail.com>
2020-03-25 17:05:59 -0400

    First 3 Modules for 'Angular Fundamentals' course

---
+++ ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/anularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleEventClicked(data){
+    console.log("received:", data);
+  }
+
+}
